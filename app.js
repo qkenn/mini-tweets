5b6e6d65644ffc4cd1058fcfb07ffc1914277c96
qkenn <github.com.sampling990@passinbox.com>
2024-08-24 18:40:29 +0530

    create test route and it's controller

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express');
-const app = express();
+const indexRouter = require('./routers/indexRouter');
 
 const PORT = 3000;
+
+const app = express();
+
+app.use('/', indexRouter);
 
 app.listen(PORT, () => console.log(`server listening on port: ${PORT}`));
---
+++ controllers/indexController.js
@@ -0,0 +1,5 @@
+const getIndex = (req, res) => {
+  res.send('hello');
+};
+
+module.exports = { getIndex };
